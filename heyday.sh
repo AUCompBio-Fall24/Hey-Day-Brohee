7610b679abeb6720f8cd8942168ba33c7e04a292
mzb0226 <mzb0226@auburn.edu>
2024-10-15 21:28:00 -0500

    Update heyday.sh

--- heyday.sh
+++ heyday.sh
@@ -4,9 +4,10 @@
 
 #This script is created for Auburn's Annual "Hey Day Celebration!
 
-#Promt users to enter their name
+#this will promt users to enter their name
+#Users write their name down and will get a personalized greetings!
 
 read -p "Hey Tiger! Can you please write your name here?" name
-
+#Print statement for the User to invite for the celebration!
 echo "Hey $name! Let's celebrate Hey Day together!"
 echo "War Eagle, $name!"
