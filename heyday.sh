81b53ce9a91ac8fa28a952d032aba50b2de54e0e
mzb0226 <mzb0226@auburn.edu>
2024-10-15 20:50:09 -0500

    Add files via upload
    Script for Hey Day

---
+++ heyday.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+#Hey day script by Brohee
+
+#This script is created for Auburn's Annual "Hey Day Celebration!
+
+#Promt users to enter their name
+
+read -p "Hey Tiger! Can you please write your name here?" name
+
+echo "Hey $name! Let's celebrate Hey Day together!"
+echo "War Eagle, $name!"
